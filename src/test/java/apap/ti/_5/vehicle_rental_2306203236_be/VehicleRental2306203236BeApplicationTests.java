4549ad7f59b7bf131179ed523493a255bca2c8c0
Kadek Savitri <kadek.savitri@ui.ac.id>
2025-10-30 09:53:10 +0700

    TI: initialize project

---
+++ src/test/java/apap/ti/_5/vehicle_rental_2306203236_be/VehicleRental2306203236BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.vehicle_rental_2306203236_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleRental2306203236BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

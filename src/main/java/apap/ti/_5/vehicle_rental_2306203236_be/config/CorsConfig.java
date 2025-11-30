ca68a1bc947346c183cf061d361a98033f042882
Kadek Savitri <kadek.savitri@ui.ac.id>
2025-11-30 12:58:10 +0700

    edit cors

--- src/main/java/apap/ti/_5/vehicle_rental_2306203236_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/vehicle_rental_2306203236_be/config/CorsConfig.java
@@ -4,18 +4,29 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 public class CorsConfig {
+
+    // @Value("${CORS_ALLOWED_ORIGINS}")
+    // private String allowedOrigins;
+
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+                // String[] origins = allowedOrigins.split(",");
+
                 registry
                     .addMapping("/**")
                     // For development allow all origins (use allowedOriginPatterns to permit credentials)
-                    .allowedOriginPatterns("*")
+                    //.allowedOriginPatterns("*")
+                    .allowedOrigins("http://localhost:5173", "http://localhost:8080", "http://2306203236-be.hafizmuh.site",
+                                "https://2306203236-fe.hafizmuh.site")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                     .allowedHeaders("*")
                     .allowCredentials(true)

9b606035c59863b7c85a708b448c3270838a3134
Kadek Savitri <kadek.savitri@ui.ac.id>
2025-11-30 21:58:57 +0700

    adjust

--- src/main/java/apap/ti/_5/vehicle_rental_2306203236_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/vehicle_rental_2306203236_be/config/CorsConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.beans.factory.annotation.Value;
-
-
 @Configuration
 public class CorsConfig {
 
